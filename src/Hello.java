6a4ca741db6a5a3e1e88396a7a8f95576d2b60ac
Reyna Navarro <navarroreyna1976@gmail.com>
2020-11-10 12:53:28 -0500

    added more comments on how to push to github.com

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,10 @@ public class Hello {
 
     //more stuff
     //Mac user and window users are happy to see commit dialog finally
-
+    // I have already published on gitHub.com
+    // now I am ready to make more commits
+    // but do I need to share again????
+    // ABSOLUTELY NO
+    // IT'S TIME COMMIT AND JUST PUSH!!!!!!
 
 }
